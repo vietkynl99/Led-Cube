73d8edbedbab50e8ec6932b23bc81326f57d36fa
vietkynl99 <vietdohoang1999@gmail.com>
2023-07-10 23:05:57 +0700

    [Android App] Change data format part2

--- android/Led-Cube-App/app/src/main/java/com/kynl/ledcube/manager/EffectManager.java
+++ android/Led-Cube-App/app/src/main/java/com/kynl/ledcube/manager/EffectManager.java
@@ -166,7 +166,7 @@ public class EffectManager {
         try {
             List<OptionItem> optionItemList = effectItemList.get(position).getOptionItemList();
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("type", String.valueOf(effectItemList.get(position).getType().ordinal()));
+            jsonObject.put("type", effectItemList.get(position).getType().ordinal());
             for (OptionItem item : optionItemList) {
                 jsonObject.put(item.getText().toLowerCase(), item.getValue());
             }
